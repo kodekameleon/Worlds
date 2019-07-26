49ee6beaaf0fe99db670f8710d044725cf9b7faf
Justin Joyce <jjoyce@aptos.com>
2019-07-26 15:46:58 +1000

    Added precss and css liniting

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+const _ = require("lodash");
 const gulp = require("gulp");
 const gulp_rename = require("gulp-rename");
 const gulp_postcss = require("gulp-postcss");
@@ -10,8 +12,13 @@ const rimraf = require("rimraf");
 const rollup = require("rollup");
 const rollup_babel = require("rollup-plugin-babel");
 const rollup_resolve = require("rollup-plugin-node-resolve");
-const postcss_assets = require("postcss-assets");
-const postcss_import = require("postcss-import");
+const postcssAssets = require("postcss-assets");
+const postcssImport = require("postcss-import");
+const postcssPrecss = require("precss");
+const postcssReporter = require("postcss-reporter");
+const autoprefixer = require("autoprefixer");
+const gulpStyleLint = require("gulp-stylelint");
+/* eslint-enable no-undef */
 
 const target = "dist";
 
@@ -59,7 +66,7 @@ gulp.task("build-html", (cb) => {
 
 //  ======== BUILD JS ========
 
-gulp.task("lint", (cb) => {
+gulp.task("lint-js", (cb) => {
   const steps = [
     gulp.src(["src/**/*.jsx", "src/**/*.js"]),
     gulp_eslint(),
@@ -69,7 +76,7 @@ gulp.task("lint", (cb) => {
   pump(steps, cb);
 });
 
-gulp.task("build-js", ["lint"], () => {
+gulp.task("build-js", ["lint-js"], () => {
   return rollup.rollup({
     input: "src/main.jsx",
     plugins: [
@@ -114,15 +121,26 @@ gulp.task("build-js", ["lint"], () => {
 
 //  ======== BUILD CSS ========
 
-gulp.task("build-css", (cb) => {
+gulp.task("lint-css", () => {
+  return gulp.src(["src/**/*.css"])
+    .pipe(gulpStyleLint({
+      failAfterError: true,
+      reporters: [{formatter: styleLintFormatter, console: true}]
+    }));
+});
+
+gulp.task("build-css", ["lint-css"], (cb) => {
   const steps = [
     gulp.src("./src/main.css", {base: "./"}),
     gulp_sourcemaps.init(),
     gulp_postcss([
-      postcss_import({from: "./src/main.css"}),
-      postcss_assets({
+      postcssImport({from: "./src/main.css"}),
+      postcssPrecss(),
+      autoprefixer(),
+      postcssAssets({
         loadPaths: ["media", "."]
-      })
+      }),
+      postcssReporter({clearReportedMessages: true})
     ]),
     gulp_rename("main.css"),
     gulp_sourcemaps.write("."),
@@ -140,145 +158,13 @@ gulp.task("watch", () => {
 });
 
 
-// var gulp = require('gulp');
-// var gulp_if = require('gulp-if');
-// var gulp_concat = require('gulp-concat');
-// var gulp_rename = require('gulp-rename');
-// var gulp_uglify = require('gulp-uglify');
-// var gulp_sourcemaps = require('gulp-sourcemaps');
-// var gulp_print = require('gulp-print');
-// var gulp_sass = require('gulp-sass');
-// var gulp_inject = require('gulp-inject');
-// var gulp_flatten = require('gulp-flatten');
-// var gulp_replace = require('gulp-replace');
-// var gulp_htmlmin = require('gulp-htmlmin');
-// var gulp_copy = require('gulp-copy');
-// var pump = require('pump');
-// var fs_extra = require('fs-extra');
-// var streamqueue = require('streamqueue');
-//
-// var task_chain = [];
-//
-// process.chdir('C:\\Dev\\svn\\platform\\branches\\4-iam\\platform\\dbcore');
-//
-// gulp.task('compile-javascript', function (cb) {
-//     var steps = [
-//         streamqueue(
-//             { objectMode: true },
-//             gulp.src(
-//                 [
-//                     "src/main/javascript/3rd-party/jquery.js",
-//                     "src/main/javascript/3rd-party/**/*.js",
-//                 ],
-//                 {"base": "src/main/javascript"}
-//             ),
-//             gulp.src(
-//                 [
-//                     "../lib/src/main/javascript/lib/**/*.js",
-//                 ],
-//                 {"base": "../lib/src/main/javascript"}
-//             ),
-//             gulp.src(
-//                 [
-//                     "src/main/javascript/**/*.js",
-//                     "!src/main/javascript/3rd-party/**/*.js",
-//                 ],
-//                 {"base": "src/main/javascript"}
-//             )
-//         ),
-//         gulp_print(),
-//         gulp_sourcemaps.init(),
-//         gulp_if("true"!=="true", gulp_uglify()),
-//         gulp_concat("dbcore.js"),
-//         gulp_if("true"==="true", gulp_sourcemaps.write('.')),
-//         gulp.dest("target/dbcore")
-//     ];
-//     pump(steps, cb);
-// });
-// task_chain.push('compile-javascript');
-//
-// gulp.task('compile-sass', function (cb) {
-//     var steps = [
-//         streamqueue(
-//             { objectMode: true },
-//             gulp.src(
-//                 [
-//                     "src/main/javascript/main.scss",
-//                     "src/main/javascript/3rd-party/**/*.*css",
-//                 ],
-//                 {"base": "src/main/javascript"}
-//             ),
-//             gulp.src(
-//                 [
-//                     "../lib/src/main/javascript/lib/**/*.scss",
-//                     "!../lib/src/main/javascript/lib/**/*.mixin.*css",
-//                 ],
-//                 {"base": "../lib/src/main/javascript"}
-//             ),
-//             gulp.src(
-//                 [
-//                     "src/main/javascript/**/*.scss",
-//                     "!src/main/javascript/main.scss",
-//                     "!src/main/javascript/3rd-party/**/*.*css",
-//                     "!src/main/javascript/**/*.mixin.*css",
-//                 ],
-//                 {"base": "src/main/javascript"}
-//             )
-//         ),
-//         gulp_print(),
-//         gulp_sourcemaps.init(),
-//         gulp_sass({ includePaths: [
-//                 'src/main/javascript/mixins',
-//                 '.',
-//                 '../lib/src/main/javascript/lib',
-//                 '../lib/src/main/javascript/lib/mixins',
-//             ]}),
-//         gulp_concat("dbcore.css"),
-//         gulp_if("true"!=="true", gulp_uglify()),
-//         gulp_if("true"==="true", gulp_sourcemaps.write('.')),
-//         gulp.dest("target/dbcore")
-//     ];
-//     pump(steps, cb);
-// });
-// task_chain.push('compile-sass');
-//
-// gulp.task('compile-html', function (cb) {
-//     var steps = [
-//         gulp.src("src/main/javascript/main.html", {base: "src/main/javascript"}),
-//         gulp_print(function(path) { return 'injecting ' + path; }),
-//         gulp_replace('$'+'{project.name}', 'dbcore'),
-//         gulp_replace('$'+'{page.title}', 'Racemi CMDB Management'),
-//         gulp_inject(
-//             streamqueue(
-//                 { objectMode: true },
-//                 gulp.src(
-//                     [
-//                         "../lib/src/main/javascript/lib/**/*.hbs",
-//                     ],
-//                     {"base": "../lib/src/main/javascript"}
-//                 ),
-//                 gulp.src(
-//                     [
-//                         "src/main/javascript/**/*.hbs",
-//                     ],
-//                     {"base": "src/main/javascript"}
-//                 )
-//             )
-//                 .pipe(gulp_print(function(path) { return 'injecting ' + path; }))
-//                 .pipe(gulp_htmlmin({collapseWhitespace: true, processScripts: ["text/x-handlebars-template"]})),
-//             {
-//                 starttag: '<!-- inject:html -->',
-//                 transform: function (filePath, file) {
-//                     return file.contents.toString('utf8')
-//                 }
-//             }
-//         ),
-//         gulp_flatten(),
-//         gulp_rename("dbcore.html"),
-//         gulp.dest("target/dbcore")
-//     ];
-//     pump(steps, cb);
-// });
-// task_chain.push('compile-html');
-//
-// gulp.task('default', task_chain);
+//  ======== Helpers ========
+
+const styleLintFormatter = results =>
+  _.flatMap(results, result =>
+    _.map(
+      result.warnings,
+      warning =>
+        `${result.source}(${warning.line},${warning.column}): ${warning.severity}: ${warning.text}`
+    )
+  ).join("\n");
